65b32dbf5d42ad4a9487841195ac7de74047ac9c
Rayhanali-dev <rayhanalidev@gmail.com>
2023-06-16 01:42:14 +0600

    create users data

--- index.js
+++ index.js
@@ -86,6 +86,17 @@ async function run() {
             res.send(result);
         });
 
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists' })
+            }
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
